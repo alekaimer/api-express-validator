593cba44c407bcae151b742fd585a0b10b29b333
Alexandre Kaimer <alekaimer@gmail.com>
2024-10-11 16:01:08 -0300

    fix: Change fn names and refactor logical of example

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { body, validationResult } - require("express-validator");
-const bodyParser - require("body-parser");
+const { body, validationResult } = require("express-validator");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000
@@ -9,8 +9,8 @@ app.use(bodyParser.json())
 
 // Single validation
 app.post("/register/v1/user", [
-  body("username").isEmail();
-  body("password").isLenght({min: 5});
+  body("username").isEmail(),
+  body("password").isLength({min: 5})
 ], (request, response) => {
   const errors = validationResult(request)
 
@@ -23,8 +23,8 @@ app.post("/register/v1/user", [
 
 // Validation with custom message (use .withMessage)
 app.post("/register/v2/user", [
-  body("username").isEmail().withMessage("O e-mail não é válido.");
-  body("password").isLenght({min: 5}).withMessage("A senha deve conter ao menos 5 caracteres.");;
+  body("username").isEmail().withMessage("O e-mail não é válido."),
+  body("password").isLength({min: 5}).withMessage("A senha deve conter ao menos 5 caracteres.")
 ], (request, response) => {
   const errors = validationResult(request)
 
@@ -59,13 +59,21 @@ app.post("/register/v3/user", [
 
 //Register User API validation example
 app.post("/register/v3/user", [
-  body("username").isEmail().withMessage("O e-mail não é válido.");
-  body("username").custom(value => {
-    //request para DB para ver se existe ou não o e-mail. supondo que já exista
-    if (value === "teste@teste.com") {
+  body("email").isEmail().withMessage("O e-mail não é válido."),
+  body("username").custom(usernameValue => {
+    // Aui deve ser realizada a requisição para o Banco de Dados para verificação de existencia ou não do e-mail.
+    // Supondo que o e-mail já exista, segue um exemplo de comparação
+    const returnOfDatabaseRequest = "teste@teste.com" 
+    if (usernameValue === returnOfDatabaseRequest) {
       return Promise.reject("E-mail já cadastrado.");
     }
-  })
+
+    return true;
+  }),
+  body("password").isLength({min: 5}).withMessage("A senha deve conter ao menos 5 caracteres."),
+  body("name").isLength({min: 2}).withMessage("O nome deve conter ao menos 2 caracteres."),
+  body("surname").isLength({min: 2}).withMessage("O sobrenome deve conter ao menos 2 caracteres."),
+  body("age").isNumeric().withMessage("A idade deve ser um número."),
 ], (request, response) => {
   const errors = validationResult(request)
 
